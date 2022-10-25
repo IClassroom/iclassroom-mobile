c8d34124fb7dee32561a2149dba01752eabf1e34
Yago Taveiros Ferreira <ytf@ic.ufal.br>
2022-10-25 17:29:07 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/ic/ufal/iclassroom/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ic.ufal.iclassroom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/api/main_service.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+abstract class MainService {
+  Map<String, String> headers = {
+    'Authorization': 'Basic AMXALINDAN/ASDKMALKM123.12.123KMO',
+  };
+
+  Future<Map<String, dynamic>> get(String path, {String? host}) async {
+    Uri uri = Uri.https(host ?? 'https://localhost:8080', path);
+    http.Response response = await http.get(uri, headers: headers);
+    return validateResponse(response);
+  }
+
+  Future<Map<String, dynamic>> post(
+    String path, {
+    String? host,
+    Map<String, dynamic>? body,
+  }) async {
+    Uri uri = Uri.https(host ?? 'https://localhost:8080', path);
+    http.Response response = await http.post(uri, body: body, headers: headers);
+    return validateResponse(response);
+  }
+
+  Future<Map<String, dynamic>> put(
+    String path, {
+    String? host,
+    Map<String, dynamic>? body,
+  }) async {
+    Uri uri = Uri.https(host ?? 'https://localhost:8080', path);
+    http.Response response = await http.put(uri, body: body, headers: headers);
+    return validateResponse(response);
+  }
+
+  Future<Map<String, dynamic>> delete(
+    String path, {
+    String? host,
+    Map<String, dynamic>? body,
+  }) async {
+    Uri uri = Uri.https(host ?? 'https://localhost:8080', path);
+    http.Response response = await http.delete(uri, body: body, headers: headers);
+    return validateResponse(response);
+  }
+
+  Map<String, dynamic> validateResponse(http.Response response) {
+    if (response.statusCode > 399) {
+      throw Exception('ResponseError');
+    }
+    return jsonDecode(response.body);
+  }
+}
